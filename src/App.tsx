e9a31440979f3dbab685d59a3bc809ecb96a3581
Wellington Albino <well.renato@hotmail.com>
2024-08-02 18:36:47 -0300

    Criando component Home

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Home } from "./pages/home/Home";
+import './App.css'
+
+export function App() {
+  return (
+    <Home />
+  )
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import './Home.css'
+
+export function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img 
+        src="https://i.imgur.com/H88yIo2.png" 
+        alt="Imagem tela inicial" 
+        className="img" 
+      />
+    </>
+  )
+}

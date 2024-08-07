e22ea8fdd9f5209bb37f990588eb4cc3618288b2
Wellington Albino <well.renato@hotmail.com>
2024-08-07 17:45:25 -0300

    Atualizando o blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { Home } from "./pages/home/Home";
-import './App.css'
+import { Home } from "./pages/Home/Home";
+import { Navbar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
 
 export function App() {
   return (
-    <Home />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+export function Login() {
+  return (
+    <>
+      <div className="h-96 mb-96">
+
+        <h1 className='text-xl'>Login</h1>
+
+      </div>
+    </>
+  );
+}

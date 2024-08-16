d4540907542f8aa526c5ead3e3010366326daec2
Wellington Albino <well.renato@hotmail.com>
2024-08-16 20:31:02 -0300

    Finalizando o projeto

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-1ltk.onrender.com/'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 // eslint-disable-next-line @typescript-eslint/ban-types
